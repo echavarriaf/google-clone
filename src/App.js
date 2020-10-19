bce839a8c1936458fc17fb8e68875d9f6106f9b1
Felix Echavarria <echavarria.f@gmail.com>
2020-10-19 14:42:07 -0400

    Second initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import Search from "./pages/Search"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Programmers</h1>
+      <Router>
+        <Switch>
+        <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
