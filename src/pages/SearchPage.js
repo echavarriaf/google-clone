419ba885fdfff333cac3ee6d4c203bdef3d6f14e
Felix Echavarria <echavarria.f@gmail.com>
2020-10-21 14:29:22 -0400

    Style the search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import "../css/SearchPage.css"
-import useGoogleSearch from '../hook/useGoogleSearch';
-import { useStateValue } from '../state/StateProvider';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/SearchPage.css";
+import useGoogleSearch from "../hook/useGoogleSearch";
+import Response from "../response";
+import { useStateValue } from "../state/StateProvider";
+import Search from "./Search";
 
 // https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
 
@@ -11,20 +13,32 @@ import { useStateValue } from '../state/StateProvider';
 // 70f57dab46b6acb89
 
 function SearchPage() {
-    const [{ term }, dispatch] = useStateValue();
-    const { data } = useGoogleSearch(term);
+  const [{ term }, dispatch] = useStateValue();
 
-    console.log(data);
-    return (
-        <div className="searchPage">
-            <div className="searchPage__header">
-                <h1>{term}</h1>
-            </div>
-            <div className="searchPage__result">
+  //LIVE API CALL
+  // const { data } = useGoogleSearch(term);
 
-            </div>
+  const data = Response;
+
+  console.log(data);
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt="Google Logo"
+          />
+        </Link>
+
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
         </div>
-    )
+      </div>
+      <div className="searchPage__results"></div>
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
