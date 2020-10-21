16421d870d72cbad0edfc365aa57ee0851b3cd88
Felix Echavarria <echavarria.f@gmail.com>
2020-10-21 18:51:10 -0400

    Finishing the app and deployed it

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -11,6 +11,7 @@ import ImageIcon from "@material-ui/icons/Image";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import RoomIcon from "@material-ui/icons/Room";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { InsertEmoticon } from "@material-ui/icons";
 
 // https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
 
@@ -22,9 +23,10 @@ function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
 
   //LIVE API CALL
-  // const { data } = useGoogleSearch(term);
+  const { data } = useGoogleSearch(term);
 
-  const data = Response;
+    //Mock API CALL
+//   const data = Response;
 
   console.log(data);
   return (
@@ -79,7 +81,7 @@ function SearchPage() {
           </div>
         </div>
       </div>
-      {true && (
+      {term && (
         <div className="searchPage__results">
           <p className="searchPage__resultCount">
             About {data?.searchInformation.formattedTotalResults} results (
@@ -88,7 +90,13 @@ function SearchPage() {
 
           {data?.items.map((item) => (
             <div className="searchPage__result">
-                  <a href={item.link}>{item.displayLink} &#9661;
+                  <a className="searchPage__resultLink" href={item.link}>
+                      {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
+                          <img className="searchPage__resultImage" src={
+                              item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src } alt=""/>
+                  
+                      )}
+                      {item.displayLink} &#9661;
                       </a>
                   <a className="searchPage__resultTitle" href={item.link}>
                       <h2>{item.title}</h2>
